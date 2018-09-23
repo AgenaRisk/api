17f235513a3cf53f180f9aa1d28cbe2113259dc3
Eugene Dementiev <dementiev.eugene@gmail.com>
2018-09-23 04:19:05 +0100

    Minor update for Model and comments

--- src/com/agenarisk/api/model/Model.java
+++ src/com/agenarisk/api/model/Model.java
@@ -19,15 +19,27 @@ import uk.co.agena.minerva.util.Environment;
 import uk.co.agena.minerva.util.io.FileHandlingException;
 
 /**
- *
+ * Model class represents an AgenaRisk model that may contain a number of Bayesian networks, scenarios etc, equivalent to com.agenarisk.api.model.Model in AgenaRisk Java API v1
  * @author Eugene Dementiev
  */
 public class Model implements IDContainer<ModelException, Network>, Storable {
 	
+	/**
+	 * ID-Network map of this Model
+	 * This should not be directly returned to other components and should be modified only by this class in a block synchronized on IDContainer.class
+	 */
 	private final Map<String, Network> networks = Collections.synchronizedMap(new HashMap<>());
 	
+	/**
+	 * The underlying logical Model
+	 */
 	private final uk.co.agena.minerva.model.Model logicModel;
 	
+	/**
+	 * Constructor for Model class
+	 * The Model is created without Scenarios or Networks
+	 * To be used by Model factory method
+	 */
 	private Model(){
 		
 		String outputMode = "system";
@@ -45,19 +57,41 @@ public class Model implements IDContainer<ModelException, Network>, Storable {
 		}
 	}
 	
+	/**
+	 * Factory method to create an empty instance of a Model class
+	 * @return new instance of a Model
+	 */
 	public static Model createModel(){
 		return new Model();
 	}
 	
-	public static Model createFromJSON(JSONObject json){
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	/**
+	 * Factory method to create an instance of a Model from provided JSONObject
+	 * Creates all member components
+	 * @param json JSONObject representing this model, including structure, tables, graphics etc
+	 * @return Model object
+	 */
+	public static Model createModel(JSONObject json){
+		throw new UnsupportedOperationException("Not supported yet.");
 	}
 	
-	@Override
-	public JSONObject toJSONObject() {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	/**
+	 * Creates a Network and adds it to this Model
+	 * Creates all member components
+	 * @param json JSONObject representing the network, including structure, tables, graphics etc
+	 * @return Network object
+	 */
+	public Network createNetwork(JSONObject json) {
+		throw new UnsupportedOperationException("Not supported yet.");
 	}
 	
+	/**
+	 * Creates a new empty Network and adds it to this Model
+	 * @param id unique ID of the Network
+	 * @param name non-unique name of the Network
+	 * @return the Network instance added to this Model
+	 * @throws ModelException if a Network with this ID already exists
+	 */
 	public Network createNetwork(String id, String name) throws ModelException {
 		synchronized (IDContainer.class){
 			if (networks.containsKey(id)){
@@ -80,8 +114,13 @@ public class Model implements IDContainer<ModelException, Network>, Storable {
 		return network;
 	}
 	
-	public Network createNetwork(JSONObject json) throws ModelException {
-		throw new ModelException("Not implemented");
+	/**
+	 * Creates a JSON representing this Network, ready for file storage
+	 * @return JSONObject representing this Network
+	 */
+	@Override
+	public JSONObject toJSONObject() {
+		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
 	/**
@@ -112,17 +151,12 @@ public class Model implements IDContainer<ModelException, Network>, Storable {
 	public void throwOldIdNullException(String id) throws ModelException {
 		throw new ModelException("Can't change Network ID to `" + id + "` because the Network does not exist in this Model or old ID is null");
 	}
-
 	
-//	public static void load(String filePath) throws FileHandlingException, JSONException {
-//		String XMLString = FileHandler.readFileAsString(filePath, Charset.forName("UTF-8"));
-//		XML2JSONConverter.parseXMLAsJSON(XMLString);
-//	}
-//	
-//	public Model(JSONObject jsonModel){
-//		
-//	}
-	
+	/**
+	 * Looks up and returns a Network by its ID
+	 * @param id the ID of the Network to return
+	 * @return the Network identified by ID or null, if no such Network exists in this Model
+	 */
 	public Network getNetwork(String id){
 		return networks.get(id);
 	}
@@ -135,10 +169,18 @@ public class Model implements IDContainer<ModelException, Network>, Storable {
 		return new HashMap<>(networks);
 	}
 
+	/**
+	 * Returns the underlying ExtendedBN
+	 * @return the underlying ExtendedBN
+	 */
 	protected uk.co.agena.minerva.model.Model getLogicModel() {
 		return logicModel;
 	}
 	
+	/**
+	 * Triggers propagation in this model for all Networks and Scenarios
+	 * @throws CalculationException if calculation failed
+	 */
 	public void calculate() throws CalculationException {
 
 		// Temp
@@ -158,6 +200,11 @@ public class Model implements IDContainer<ModelException, Network>, Storable {
 		}
 	}
 	
+	/**
+	 * Saves the Model to a file path specified in the old CMP format
+	 * @param path the file path to save to
+	 * @throws FileIOException if saving fails
+	 */
 	public void save(String path) throws FileIOException {
 		try {
 			getLogicModel().save(path);
