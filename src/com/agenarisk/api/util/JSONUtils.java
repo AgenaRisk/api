ed6ed8716614b0a94b260f3074f5eded4441cadb
Eugene Dementiev <dementiev.eugene@gmail.com>
2018-10-09 01:25:19 +0100

    Added helper methods to JSONUtils to be used for XML to JSON conversion

--- src/com/agenarisk/api/util/JSONUtils.java
+++ src/com/agenarisk/api/util/JSONUtils.java
@@ -73,6 +73,124 @@ public class JSONUtils {
 	}
 	
 	/**
+	 * Converts an array of mixed objects into a 2D array.
+	 * <br>
+	 * The array resides under the `parentKey` in grandParent JSONObject.
+	 * <br>
+	 * Elements of the original array are JSONObjects and contain an item of key `elementKey`. This item could in turn be a JSONArray or a simple type.
+	 * <br>
+	 * After the operation, the original JSONArray will contain JSONArrays, such that the `elementKey` level is omitted.
+	 * <br>
+	 * For example,
+	 * <br>
+	 * {"table":{"probs":[{"cell":0.2},{"cell":0.8}]}}
+	 * <br>
+	 * will be converted to
+	 * <br>
+	 * {"table":{"probs":[[0.2],[0.8]]}}
+	 * <br>
+	 * And
+	 * <br>
+	 * {"table":{"probs":[{"cell":[0.2,0.7},{"cell":[0.8,0.3]}]}}
+	 * <br>
+	 * will be converted to
+	 * <br>
+	 * {"table":{"probs":[[0.2, 0.7],[0.8, 0.3]]}}
+	 * 
+	 * @param grandParent JSONObject that should contain the 2D array (in the example - object `table`)
+	 * @param parentKey the key of the 2D array in grandParent (in the example - "probs")
+	 * @param elementKey the elementKey to be omitted (in the example - "cell")
+	 * 
+	 * @throws JSONException 
+	 */
+	public static void convertTo2DArray(JSONObject grandParent, String parentKey, String elementKey) throws JSONException{
+		// E.g. table (obj), probabilities(arr), cell(string/arr)
+		JSONArray arrayOld = grandParent.getJSONArray(parentKey);
+		
+		for (int r = 0; r < arrayOld.length(); r++){
+			
+			// E.g. JSONObject that has key "cell"
+			JSONObject itemLevel1 = arrayOld.getJSONObject(r);
+			Object itemLevel2 = itemLevel1.get(elementKey);
+			
+			if (!(itemLevel2 instanceof JSONArray)){
+				// Convert array element to array
+				// E.g. "cell" only has a single item (string of double)
+				JSONArray itemLevel1New = new JSONArray();
+				itemLevel1New.put(itemLevel2);
+				arrayOld.put(r, itemLevel1New);
+			}
+			else {
+				// Otherwise just omit elementKey and replace the object with the key by this array
+				arrayOld.put(r, itemLevel2);
+			}
+			
+		}
+	}
+	
+	/**
+	 * Omits the key `childKey` and puts its contents directly under the `parentKey` as a JSONArray.
+	 * <br>
+	 * Wraps into JSONArray as necessary.
+	 * <br>
+	 * For example,
+	 * <br>
+	 * {"network":{"nodes":{"node":"X"}}}
+	 * <br>
+	 * will be converted to
+	 * <br>
+	 * {"network":{"nodes":["X"]}
+	 * <br>
+	 * And
+	 * <br>
+	 * {"network":{"nodes":{"node":["X","Y"]}}}
+	 * <br>
+	 * will be converted to
+	 * <br>
+	 * {"network":{"nodes":["X", "Y"]}
+	 * 
+	 * @param grandParent JSONObject containing `parentKey`, in the example - object network
+	 * @param parentKey - the key under which to put the resulting array, in the example - "nodes"
+	 * @param childKey - the key to omit (and wrap contents into array if necessary), in the example - "node"
+	 * 
+	 * @throws JSONException 
+	 */
+	public static void convertToJSONArray(JSONObject grandParent, String parentKey, String childKey) throws JSONException {
+		
+		JSONArray jsonArray = new JSONArray();
+		
+		if (!grandParent.has(parentKey)){
+			grandParent.put(parentKey, new JSONArray());
+			return;
+		}
+		
+		Object jsonParentObject = grandParent.get(parentKey);
+		Object jsonChildObject;
+		if (jsonParentObject instanceof JSONObject){
+			jsonChildObject = ((JSONObject)jsonParentObject).get(childKey);
+		}
+		else if (jsonParentObject instanceof JSONArray){
+			jsonChildObject = ((JSONArray)jsonParentObject).getJSONObject(0).get(childKey);
+		}
+		else if (jsonParentObject instanceof String){
+			grandParent.put(parentKey, new JSONArray());
+			return;
+		}
+		else {
+			throw new JSONException("Wrong object type for `"+parentKey+"`.`"+childKey+"`");
+		}
+		
+		if (jsonChildObject instanceof JSONArray){
+			jsonArray = (JSONArray) jsonChildObject;
+		}
+		else {
+			jsonArray.put(jsonChildObject);
+		}
+		
+		grandParent.put(parentKey, jsonArray);
+	}
+	
+	/**
 	 * Loads Model configuration from an XML file and converts it into suitable JSONObject.
 	 * 
 	 * @param path XML file path
