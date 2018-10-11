655c92ccaba5ad41d83f7c475867c97bf0451aec
Eugene Dementiev <dementiev.eugene@gmail.com>
2018-10-11 01:22:53 +0100

    AdapterException for various adapter classes

---
+++ src/com/agenarisk/api/exception/AdapterException.java
@@ -0,0 +1,17 @@
+package com.agenarisk.api.exception;
+
+/**
+ *
+ * @author Eugene Dementiev
+ */
+public class AdapterException extends AgenaRiskException {
+
+	public AdapterException(String message) {
+		super(message);
+	}
+
+	public AdapterException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+}
