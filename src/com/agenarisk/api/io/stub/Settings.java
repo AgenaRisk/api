7b2ce1b694d424e9abbe09ec9a6b8c2c356ba131
Eugene Dementiev <dementiev.eugene@gmail.com>
2018-10-11 01:12:00 +0100

    Added Settings stub class

---
+++ src/com/agenarisk/api/io/stub/Settings.java
@@ -0,0 +1,23 @@
+package com.agenarisk.api.io.stub;
+
+/**
+ * This is a stub class that only contains field values for input/output to XML and JSON format.
+ * 
+ * @author Eugene Dementiev
+ */
+public class Settings {
+	
+	/**
+	 * This is set of fields for input/output to XML and JSON format
+	 */
+	public static enum Field {
+		settings,
+		iterations,
+		convergence,
+		tolerance,
+		sampleSize,
+		discreteTails,
+		simulationLogging,
+		parameterLearningLogging
+	}
+}
