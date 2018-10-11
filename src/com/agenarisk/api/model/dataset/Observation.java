d5d40845b8bd7ebba39455d6da66ac0ea2fd53af
Eugene Dementiev <dementiev.eugene@gmail.com>
2018-10-11 23:11:19 +0100

    Observation now concrete class and does not need separating into soft and hard subclasses. Operates with a map of entries and soft/hard can be implied based on size of the map.

--- src/com/agenarisk/api/model/dataset/Observation.java
+++ src/com/agenarisk/api/model/dataset/Observation.java
@@ -1,34 +1,101 @@
 package com.agenarisk.api.model.dataset;
 
+import com.agenarisk.api.exception.AgenaRiskRuntimeException;
 import com.agenarisk.api.model.Node;
 import com.agenarisk.api.model.DataSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Observation class is a view of an observation, valid at the time of resolution and not maintained.
  * 
  * @author Eugene Dementiev
  */
-public abstract class Observation {
+public class Observation<T> {
+	
+	/**
+	 * This is set of fields for input/output to XML and JSON format
+	 */
+	public static enum Field {
+		observations,
+		observation,
+		network,
+		node,
+		entries,
+		entry,
+		value,
+		weight
+	}
 
 	/**
 	 * DataSet that contains this Observation
 	 */
-	private final DataSet dataset;
+	private final DataSet dataSet;
 	
 	/**
 	 * Observed Node
 	 */
 	private final Node node;
+	
+	/**
+	 * Map of observation entries
+	 * <br>
+	 * Hard observations will only contain one value
+	 */
+	private final Map<T, Double> entries;
 
 	/**
 	 * Constructor for the observation.
 	 * 
-	 * @param dataset DataSet that contains this Observation
+	 * @param dataSet DataSet that contains this Observation
 	 * @param node observed Node
+	 * @param entries Map of observation entries
 	 */
-	public Observation(DataSet dataset, Node node) {
+	private Observation(DataSet dataSet, Node node, Map<T, Double> entries) {
 		this.node = node;
-		this.dataset = dataset;
+		this.dataSet = dataSet;
+		this.entries = new TreeMap<>(entries);
+		
+		if (!entries.isEmpty()){
+			// Validate observation type
+			
+			Object keyElement = entries.keySet().toArray()[0];
+			switch(keyElement.getClass().getCanonicalName()){
+				case "java.lang.String":
+				case "java.lang.Integer":
+				case "java.lang.Double":
+					break;
+				default:
+					throw new AgenaRiskRuntimeException("Invalid observation value type: "+keyElement.getClass().getCanonicalName());
+			}
+		}
+		else {
+			throw new AgenaRiskRuntimeException("Empty observation not allowed");
+		}
+	}
+	
+	/**
+	 * Creates an observation (and the underlying logical structure).
+	 * 
+	 * @param <T> Integer, Double or String type of Observation
+	 * @param dataSet DataSet containing the Observation
+	 * @param node Node that the observation is for
+	 * @param entries map of entries of values with weights
+	 * 
+	 * @return created Observation instance
+	 */
+	protected static <T> Observation<T> createObservation(DataSet dataSet, Node node, Map<T, Double> entries){
+		// Create logic observation here
+		return new Observation(dataSet, node, entries);
+	}
+
+	/**
+	 * Returns a map of observation entries. Changes to the map are not tracked and have no effect on the model or dataset.
+	 * 
+	 * @return map of observation entries
+	 */
+	public Map<T, Double> getEntries() {
+		return entries;
 	}
 	
 	/**
@@ -36,7 +103,7 @@ public abstract class Observation {
 	 * @return DataSet that contains this Observation
 	 */
 	public DataSet getDataSet() {
-		return dataset;
+		return dataSet;
 	}
 
 	/**
