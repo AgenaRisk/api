dedd788329c6cc2dab8d5b787f7a9cc529d631ce
Eugene Dementiev <dementiev.eugene@gmail.com>
2018-10-13 16:23:52 +0100

    Adding Settings.loadSettings(Model model, JSONObject jsonSettings)

--- src/com/agenarisk/api/model/Settings.java
+++ src/com/agenarisk/api/model/Settings.java
@@ -1,5 +1,7 @@
 package com.agenarisk.api.model;
 
+import org.apache.sling.commons.json.JSONObject;
+
 /**
  * This is a stub class that only contains field values for input/output to XML and JSON format.
  * 
@@ -21,4 +23,17 @@ public class Settings {
 		simulationLogging,
 		parameterLearningLogging
 	}
+	
+	public static void loadSettings(Model model, JSONObject jsonSettings) {
+		uk.co.agena.minerva.model.Model logicModel = model.getLogicModel();
+		logicModel.setSimulationNoOfIterations(jsonSettings.optInt(Field.iterations.toString(), logicModel.getSimulationNoOfIterations()));
+		logicModel.setSimulationEntropyConvergenceTolerance(jsonSettings.optDouble(Field.convergence.toString(), logicModel.getSimulationEntropyConvergenceTolerance()));
+		logicModel.setSimulationEvidenceTolerancePercent(jsonSettings.optDouble(Field.tolerance.toString(), logicModel.getSimulationEvidenceTolerancePercent()));
+		logicModel.setSampleSize(jsonSettings.optInt(Field.sampleSize.toString(), logicModel.getSampleSize()));
+		logicModel.setRankedSampleSize(jsonSettings.optInt(Field.sampleSizeRanked.toString(), logicModel.getRankedSampleSize()));
+		logicModel.setSimulationTails(jsonSettings.optBoolean(Field.discreteTails.toString(), logicModel.isSimulationTails()));
+		logicModel.setSimulationLogging(jsonSettings.optBoolean(Field.simulationLogging.toString(), logicModel.isSimulationLogging()));
+		logicModel.setEMLogging(jsonSettings.optBoolean(Field.parameterLearningLogging.toString(), logicModel.isEMLogging()));
+	}
+	
 }
