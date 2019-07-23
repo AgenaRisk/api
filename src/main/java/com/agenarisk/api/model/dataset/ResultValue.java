8e1e1b6bd6a639bfc2decc41ba6123e15cb77dac
Eugene Dementiev <dementiev.eugene@gmail.com>
2019-07-23 23:59:08 +0100

    Added toJson and toString

--- src/main/java/com/agenarisk/api/model/dataset/ResultValue.java
+++ src/main/java/com/agenarisk/api/model/dataset/ResultValue.java
@@ -1,6 +1,7 @@
 package com.agenarisk.api.model.dataset;
 
 import com.agenarisk.api.model.CalculationResult;
+import org.json.JSONObject;
 
 /**
  * ResultValue represents a state or range with probability mass allocated to it.
@@ -74,4 +75,36 @@ public class ResultValue {
 		return value;
 	}
 	
+	/**
+	 * Returns a JSON representation of this object.
+	 * 
+	 * @return JSON
+	 */
+	public JSONObject toJson(){
+		JSONObject json = new JSONObject();
+		json.put(Field.label.toString(), label);
+		json.put(Field.value.toString(), value);
+		return json;
+	}
+	
+	/**
+	 * Returns a String value of the JSON representation of this object.
+	 * 
+	 * @return JSON string
+	 */
+	@Override
+	public String toString(){
+		return toJson().toString();
+	}
+	
+	/**
+	 * Returns a String value of the JSON representation of this object.
+	 * 
+	 * @param indentFactor Larger number indicates the depth to which new lines and tabs should be used to indent the JSON string
+	 * 
+	 * @return Indented JSON string
+	 */
+	public String toString(int indentFactor){
+		return toJson().toString(indentFactor);
+	}
 }
