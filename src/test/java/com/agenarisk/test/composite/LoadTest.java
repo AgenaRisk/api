207a9b63d07a2945a1d931986874d406007c121d
Eugene Dementiev <dementiev.eugene@gmail.com>
2019-10-21 18:22:17 +0100

    Adding a test for loading AgenaRisk 7 style CMP file

--- src/test/java/com/agenarisk/test/composite/LoadTest.java
+++ src/test/java/com/agenarisk/test/composite/LoadTest.java
@@ -1,6 +1,8 @@
 package com.agenarisk.test.composite;
 
+import com.agenarisk.api.model.Model;
 import com.agenarisk.test.TestHelper;
+import java.nio.file.Path;
 import org.junit.Test;
 
 /**
@@ -16,5 +18,15 @@ public class LoadTest {
 	public void testLoadInputNodeTable() throws Exception {
 		TestHelper.loadModelFromResource("/load/LoadInputNodeTable.xml").calculate();
 		
+	}
+	
+	/**
+	 * This test makes sure that this API can load and calculate an AgenaRisk 7 CMP model
+	 */
+	@Test
+	public void testLoadCMP() throws Exception {
+		Path tempPath = TestHelper.tempFileCopyOfResource("/load/ar7-asia.cmp");
+		Model model = Model.loadModel(tempPath.toString());
+		model.calculate();
 	}
 }
