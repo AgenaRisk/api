2b2c1be8863ea00c389650e4b44654d1c5f88157
Eugene Dementiev <dementiev.eugene@gmail.com>
2019-11-11 06:08:08 +0000

    Fix imports

--- src/main/java/com/agenarisk/api/model/Model.java
+++ src/main/java/com/agenarisk/api/model/Model.java
@@ -32,6 +32,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.json.JSONArray;
 import org.json.JSONException;
