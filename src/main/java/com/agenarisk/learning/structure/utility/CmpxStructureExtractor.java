2c5246973794f07a9264be18fd0186f3c4d56746
Eugene Dementiev <dementiev.eugene@gmail.com>
2025-02-21 02:55:58 +0000

    https://agenarisk.atlassian.net/browse/JAPI-153?focusedCommentId=16146

---
+++ src/main/java/com/agenarisk/learning/structure/utility/CmpxStructureExtractor.java
@@ -0,0 +1,36 @@
+package com.agenarisk.learning.structure.utility;
+
+import com.agenarisk.api.model.Model;
+import com.agenarisk.learning.structure.logger.BLogger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import uk.co.agena.minerva.model.extendedbn.ExtendedBN;
+import uk.co.agena.minerva.model.extendedbn.ExtendedNode;
+
+/**
+ *
+ * @author Eugene Dementiev
+ */
+public class CmpxStructureExtractor {
+	public static ArrayList<List<Object>> extract(Model model){
+		ArrayList<List<Object>> lines = new ArrayList<>();
+		lines.add(Arrays.asList("ID", "Variable 1", "Dependency", "Variable 2"));
+		
+		try {
+			int i = 1;
+			ExtendedBN ebn = model.getNetworkList().get(0).getLogicNetwork();
+			for(ExtendedNode en: (List<ExtendedNode>) ebn.getExtendedNodes()){
+				for (ExtendedNode enParent: (List<ExtendedNode>)ebn.getParentNodes(en)){
+					lines.add(Arrays.asList(String.valueOf(i), enParent.getConnNodeId(), "->", en.getConnNodeId()));
+					i+=1;
+				}
+			}
+		}
+		catch (Exception ex){
+			BLogger.logConditional(ex);
+		}
+		
+		return lines;
+	}
+}
